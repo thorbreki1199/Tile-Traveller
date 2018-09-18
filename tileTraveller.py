7e9c44a77bba38582041d8aa84201fb9b265d090
thorbreki <40344038+thorbreki@users.noreply.github.com>
2018-09-18 13:18:11 +0000

    Bætti við algrímið

---
+++ tileTraveller.py
@@ -0,0 +1,14 @@
+'''
+Byrja að setja breytu x og y sem 1
+
+Á meðan spilari er ekki á reit 3,1:
+	Ná hvaða leiðum spilarinn má fara (með if og elif)
+	Skrifa út mögulegar leiðir
+	Fá inntak frá spilara
+	Ef spilarinn slær inn svar sem er gilt:
+		Breyta x og y gildum
+	Annars:
+		Prenta út villumeldingu
+
+Prenta út “Victory!”
+'''
